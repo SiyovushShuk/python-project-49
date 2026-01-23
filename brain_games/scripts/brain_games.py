c846b475cbe476480d5a7c6dc21490b14cbdc0b1
Seva <shukurovsiyovush1@gmail.com>
2026-01-23 16:56:12 +0500

    fix brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,12 @@
 from brain_games.cli import welcome_user
 
+
 def main():
     print('Welcome to the Brain Games!')
 
     username = welcome_user()
 
     print(f'Hello, {username}!')
-
-
 
 
 if __name__ == "__main__":
