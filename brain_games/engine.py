859678904c427a8cd93c7c67dae1dcfa7189c5da
Seva <shukurovsiyovush1@gmail.com>
2026-02-02 11:44:32 +0500

    Add engine logic

---
+++ brain_games/engine.py
@@ -0,0 +1,28 @@
+import random
+
+import prompt
+
+
+def make_random_number() -> int:
+
+    return random.randint(1, 99)
+
+
+def start_game(username: str, game) -> None:
+
+    count_queastions = 3
+
+    for i in range(count_queastions):
+
+        correct_answer = game()
+
+        user_answer = prompt.integer('Your answer: ')
+
+        if correct_answer == user_answer:
+            print('Correct!')
+        else:
+            print(f"{user_answer} is wrong answer ;(. Correct answer was '{correct_answer}'.")  # noqa: E501
+            print(f"Let's try again, {username}!")
+            break
+    else:
+        print(f'Congratulations, {username}!')
