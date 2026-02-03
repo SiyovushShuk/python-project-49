22e40fd75331c7dd756c310e4ac436be452484da
Seva <shukurovsiyovush1@gmail.com>
2026-02-03 12:12:33 +0500

    Add game brain progression

---
+++ brain_games/games/brain_progression.py
@@ -0,0 +1,3 @@
+def brain_progression():
+    a = 0
+    return a
---
+++ brain_games/scripts/brain-progression.py
@@ -0,0 +1,15 @@
+from brain_games.cli import welcome_user
+from brain_games.engine import start_game
+from brain_games.games.brain_progression import brain_progression
+
+
+def main():
+
+    username = welcome_user()
+    print('Find the greatest common divisor of given numbers.')
+
+    start_game(username, brain_progression)
+
+
+if __name__ == "__main__":
+    main()
