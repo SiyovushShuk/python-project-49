95f7c4d88a949114d7b2ead3c3fc54fbc136a3b6
Seva <shukurovsiyovush1@gmail.com>
2026-02-03 17:49:34 +0500

    Refactor engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,12 +1,38 @@
 import random
+from typing import Any, Tuple
 
 
 def make_random_number() -> int:
+    '''
+    Function generate random number in the range from 0 to 99.
+    
+    :return: return random number in the range from 0 to 99.
+    :rtype: int
+    '''
 
     return random.randint(0, 99)
 
 
-def start_game(username: str, game_round) -> None:
+def start_game(
+        username: str,
+        game_round: Tuple[Any, Any],
+        count_queastions: int = 3
+        ) -> None:
+    '''
+    Function start game, check user answers.
+    Depending on the answer, the user sends a response.  
+    
+    @param username: Username of player
+    :type username: str
+
+    @param count_queastions: Numbers of game round. Default game has 3 rounds.
+    :type count_queastions: int
+
+    @param game_round: Function of game round. Realize game logic. Print question, get user answer and find correct answer. Give back in first param correct answer, second answer user answer.
+    :type game_round: Tuple[Any, Any]
+
+    :return: function sends result of processing response 
+    '''  # noqa: E501
 
     count_queastions = 3
 
