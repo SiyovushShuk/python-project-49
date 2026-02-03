86f9c1941d3850160196e6fc5d1e1122005fc398
Seva <shukurovsiyovush1@gmail.com>
2026-02-03 14:13:39 +0500

    Configure game brain progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,3 +1,53 @@
-def brain_progression():
-    a = 0
-    return a
+import random
+from typing import List, Tuple
+
+import prompt
+
+from brain_games.engine import make_random_number
+
+
+def make_answer_question(progression_row: List[int]) -> Tuple[str, int]:
+
+    random_index = random.randint(0, len(progression_row) - 1)
+
+    answer = progression_row[random_index]
+
+    progression_row[random_index] = ".."
+
+    question = " ".join(map(str, progression_row))
+    
+    return question, answer
+
+
+def make_pregression_row(
+        start_number: int, step: int, length: int
+        ) -> List[int]:
+    
+    progression_row: List[int] = []
+
+    for index in range(length):
+        current_element = start_number + index * step
+        progression_row.append(current_element)
+    
+    return progression_row
+
+
+def brain_progression() -> Tuple[int, int]:
+
+    start_number = make_random_number()
+
+    step = random.randint(1, 10)
+
+    length_progression = random.randint(5, 12)
+
+    progression_row = make_pregression_row(
+        start_number, step, length_progression
+        )
+    
+    question, correct_answer = make_answer_question(progression_row)
+
+    print(f'Question: {question}')
+
+    user_answer = prompt.integer('Your answer: ')
+
+    return correct_answer, user_answer
